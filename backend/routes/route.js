d886eed33a5c2959b31ec60b5be98e2004072878
Puspendu Das <puspendudas2002@gmail.com>
2023-09-04 21:14:03 +0530

    updated

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,29 +1,50 @@
 import express from "express";
-import { signupuser ,loginuser} from "../controller/user_controller.js";
-import { uploadImage,getImage } from "../controller/image_controller.js";
-import { createPost , getAllPosts , getPost, updatePost, deletePost} from "../controller/post_controller.js";
-import { authenticateToken } from "../controller/jwt_controller.js";
-import { createComment ,getAllComments ,deleteComment} from "../controller/comment_controller.js";
-import upload from '../utils/upload.js'
 
+import {
+  createPost,
+  updatePost,
+  deletePost,
+  getPost,
+  getAllPosts,
+} from "../controller/post-controller.js";
+import { uploadImage, getImage } from "../controller/image-controller.js";
+import {
+  getAllComments,
+  deleteComment,
+  createComment,
+} from "../controller/comment-controller.js";
+import {
+  loginUser,
+  singupUser,
+  logoutUser,
+} from "../controller/user-controller.js";
+import {
+  authenticateToken,
+  createNewToken,
+} from "../controller/jwt-controller.js";
+
+import upload from "../utils/upload.js";
 
 const router = express.Router();
 
+router.post("/login", loginUser);
+router.post("/signup", singupUser);
+router.post("/logout", logoutUser);
 
-router.post('/signup',signupuser);
-router.post('/login',loginuser);
+router.post("/token", createNewToken);
 
-router.post('/file/upload',upload.single('file'),uploadImage);
-router.get('/file/:filename',getImage)
+router.post("/create", authenticateToken, createPost);
+router.put("/update/:id", authenticateToken, updatePost);
+router.delete("/delete/:id", authenticateToken, deletePost);
 
-router.post('/create', authenticateToken, createPost);
-router.get('/posts', authenticateToken,getAllPosts);
-router.get('/post/:id', authenticateToken,getPost)
-router.put('/update/:id', authenticateToken,updatePost);
-router.delete('/delete/:id', authenticateToken,deletePost);
+router.get("/post/:id", authenticateToken, getPost);
+router.get("/posts", authenticateToken, getAllPosts);
 
-router.post('/comment/new', authenticateToken,createComment);
-router.get('/comments/:id', authenticateToken,getAllComments);
-router.delete('/comment/delete/:id', authenticateToken,deleteComment);
+router.post("/file/upload", upload.single("file"), uploadImage);
+router.get("/file/:filename", getImage);
+
+router.post("/comment/new", authenticateToken, createComment);
+router.get("/comments/:id", authenticateToken, getAllComments);
+router.delete("/comment/delete/:id", authenticateToken, deleteComment);
 
 export default router;
